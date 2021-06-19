7115f3ab5fe0431c7d9214c7092c602c6de07b49
LexiLeong <LexiLeong@users.noreply.github.com>
2021-06-20 02:20:23 +0800

    历史面试表格循环渲染

--- OPTP front-end/src/views/historyInterview.vue
+++ OPTP front-end/src/views/historyInterview.vue
@@ -7,16 +7,21 @@
 			<el-button @click="clearFilter">清除所有过滤器</el-button>
 			<el-table ref="filterTable" :data="interviewList" style="width: 100%">
 				<el-table-column prop="company" label="公司" width="180" :formatter="formatter">
+					<template slot-scope="scope">{{scope.row.company}}</template>
 				</el-table-column>
 				<el-table-column prop="title" label="名称" width="100" :formatter="formatter">
+					<template slot-scope="scope">{{scope.row.title}}</template>
 				</el-table-column>
 				<!-- <el-table-column prop="interviewer" label="面试官" :formatter="formatter">
 				</el-table-column> -->
 				<el-table-column prop="problemCount" label="题目数量" :formatter="formatter">
+					<template slot-scope="scope">{{scope.row.problemCount}}</template>
 				</el-table-column>
 				<el-table-column prop="finalResult" label="结果" :formatter="formatter">
+					<template slot-scope="scope">{{scope.row.finalResult}}</template>
 				</el-table-column>
 				<el-table-column prop="chatCount" label="留言数量" :formatter="formatter">
+					<template slot-scope="scope">{{scope.row.chatCount}}</template>
 				</el-table-column>
 			</el-table>
 		</div>
@@ -49,8 +54,10 @@
 							.then((res) => {
 								if (res.data.status == '0') {
 									console.log('1', res);
+									console.log(res.data.result.list);
 									that.tempList = res.data.result.list;
-									//console.log(that.tempList);
+									console.log(that.tempList);
+									that.loadInterviewList();
 								} else {
 									console.log('getInfo status Error!');
 								}
@@ -65,21 +72,23 @@
 			loadInterviewList() {
 				// return new Promise(resolve => {
 				//    setTimeout(() => {
-				console.log('2');
-				console.log(this.tempList);
+				// console.log('2');
+				// console.log(this.tempList);
 				let i = 0;
 				for (i = 0; i < this.tempList.length; i++) {
-					console.log(tempList[i]['company']);
+					// console.log(this.tempList[i]['company']);
 					let temp = {
-						company: tempList[i]['company'],
-						title: tempList[i]['title'],
+						company: this.tempList[i]['company'],
+						title: this.tempList[i]['title'],
 
-						problemCount: tempList[i]['problem'].length,
-						finalResult: tempList[i]['finalResult'],
-						chatCount: tempList[i]['chat'].length
+						problemCount: this.tempList[i]['problem'].length,
+						finalResult: this.tempList[i]['finalRuselt'],
+						chatCount: this.tempList[i]['chat'].length
 					}
-					this.interviewList.append(temp);
+					// console.log('temp:',temp);
+					this.interviewList.push(temp);
 				}
+				console.log(this.interviewList);
 				// }, 1000)
 				//  });
 			},
@@ -106,12 +115,11 @@
 				return row[property] === value;
 			}
 		},
-		created() {
 
-		},
 		mounted() {
 
-			this.init();
+			this.getHistoryInterview();
+			
 		}
 	}
 </script>
