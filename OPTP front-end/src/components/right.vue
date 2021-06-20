6c3fcd4eb4f5e5f17ec2598147bd2ad54905020a
ZoeDong <60535303+ZoeDong@users.noreply.github.com>
2021-06-20 13:19:43 +0800

    socketio right.vue test

--- OPTP front-end/src/components/right.vue
+++ OPTP front-end/src/components/right.vue
@@ -1,9 +1,51 @@
 <template>
 	<h4>聊天板块</h4>
+	<!-- <div style="width: 100%; float: right">
+    	输入：<input type="text" id="msginput">
+		<button id="msgbtn">发送</button>
+		<div style="width: 65%; float: right" id="showbox">
+		</div>
+	</div> -->
+	
 </template>
 
+
+<!--   -----------------  socketio test   ---------------------------   -->
+<!--
+<script src="/socket.io/socket.io.js" > </script>
+
 <script>
+    $(function () {
+        var socket = io.connect();
+    });
 </script>
+-->
+
+
+<script>
+	// var socket = io.connect('http://localhost:3000');
+	// var socket = io.connect();
+    // let btn = document.getElementById('msgbtn');
+    // let msginput = document.getElementById('msginput');
+    // let showbox = document.getElementById('showbox');
+    // btn.addEventListener('click', (event) => {
+	// 	console.log('发送信息');
+    //     let msg = msginput.value;
+    //     let data = {msg: msg};
+    //     socket.emit('sendMessage', data);
+    // });
+    // socket.on('receiveMessage', (data) => {
+    //     console.log('收到');
+    //     console.log(data);
+        
+    //     let message = document.createElement('div');
+    //     message.innerHTML = `收到的新消息是: ${data.msg}`;
+    //     showbox.appendChild(message);
+    // })
+</script>
+
+<!--   -----------------  socketio test   ---------------------------   -->
+
 
 <style>
 </style>
